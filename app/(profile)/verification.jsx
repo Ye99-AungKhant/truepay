d7ac7fa30e31d970c45ab041822732f123ea0a38
Ye99-AungKhant <yeaungkhant077@gmail.com>
2024-08-23 14:05:34 +0630

    dropdown countrylist

--- app/(profile)/verification.jsx
+++ app/(profile)/verification.jsx
@@ -1,26 +1,34 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { View, StyleSheet, ScrollView, TouchableOpacity, Image } from 'react-native';
+import { View, StyleSheet, ScrollView, TouchableOpacity, Image, Pressable } from 'react-native';
 import { Card, TextInput, Button, RadioButton, Text, IconButton } from 'react-native-paper';
-import { router, useLocalSearchParams } from 'expo-router';
+import { router } from 'expo-router';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import { MainContext } from '../provider/AppProvider';
+import { Picker } from '@react-native-picker/picker';
+import CountryList from './../../lib/CountryList';
 
 const Verification = () => {
     const [gender, setGender] = useState('');
     const [idType, setIdType] = useState('');
     const [idNo, setIdNo] = useState('');
     const [dob, setDob] = useState('');
-    const [country, setCountry] = useState('');
+    const [country, setCountry] = useState('Myanmar');
     const [city, setCity] = useState('');
     const [postalCode, setPostalCode] = useState('');
-    const searchParams = useLocalSearchParams();
     const { frontIDImg, setFrontIDImg, backIDImg, setBackIDImg } = useContext(MainContext);
+    const [date, setDate] = useState(null)
+    const [showDatePicker, setShowDatePicker] = useState(false)
 
-    useEffect(() => {
-        if (frontIDImg) {
-            console.log('front photo', frontIDImg);
-
+    const onChange = ({ type }, selectedDate) => {
+        if (type == "set") {
+            setShowDatePicker(false)
+            const currentDate = selectedDate
+            setDate(currentDate)
+            setDob(currentDate.toLocaleDateString())
+        } else {
+            setShowDatePicker(false)
         }
-    }, [frontIDImg]);
+    }
 
     return (
         <ScrollView style={styles.container}>
@@ -28,39 +36,59 @@ const Verification = () => {
             <Card style={styles.card}>
                 <Card.Title title="Personal Information" />
                 <Card.Content>
-                    <TextInput
-                        label="ID Type"
+                    <Text style={styles.genderLabel}>ID Type</Text>
+                    <RadioButton.Group
+                        onValueChange={newValue => setIdType(newValue)}
                         value={idType}
-                        onChangeText={setIdType}
-                        style={styles.input}
-                    />
+                    >
+                        <View style={styles.radioContainer}>
+                            <RadioButton value="NRC" />
+                            <Text>NRC</Text>
+                        </View>
+                        <View style={styles.radioContainer}>
+                            <RadioButton value="Passport" />
+                            <Text>Passport</Text>
+                        </View>
+                        <View style={styles.radioContainer}>
+                            <RadioButton value="Driver license" />
+                            <Text>Driver license</Text>
+                        </View>
+                    </RadioButton.Group>
                     <TextInput
                         label="ID No."
                         value={idNo}
                         onChangeText={setIdNo}
                         style={styles.input}
                     />
-                    <TextInput
-                        label="Date of Birth"
-                        value={dob}
-                        onChangeText={setDob}
-                        style={styles.input}
-                    />
+                    <Pressable onPress={() => setShowDatePicker(true)}>
+                        <TextInput
+                            label="Date of Birth"
+                            value={dob}
+                            onChangeText={setDob}
+                            style={styles.input}
+                            editable={false}
+                        />
+                    </Pressable>
+                    {showDatePicker &&
+                        <DateTimePicker
+                            mode={'date'}
+                            display={'spinner'}
+                            value={date || new Date()}
+                            onChange={onChange}
+                        />
+                    }
 
                     <Text style={styles.genderLabel}>Gender</Text>
-                    <RadioButton.Group
-                        onValueChange={newValue => setGender(newValue)}
-                        value={gender}
-                    >
-                        <View style={styles.radioContainer}>
-                            <RadioButton value="male" />
-                            <Text>Male</Text>
-                        </View>
-                        <View style={styles.radioContainer}>
-                            <RadioButton value="female" />
-                            <Text>Female</Text>
-                        </View>
-                    </RadioButton.Group>
+                    <Picker
+                        selectedValue={gender}
+                        onValueChange={(newValue, itemIndex) =>
+                            setGender(newValue)
+                        }>
+                        <Picker.Item label="Male" value="male" />
+                        <Picker.Item label="Female" value="female" />
+                        <Picker.Item label="Other" value="other" />
+                    </Picker>
+
                     <Text style={styles.genderLabel}>Upload ID Photo</Text>
 
                     <View style={styles.uploadIdContainer}>
@@ -85,12 +113,15 @@ const Verification = () => {
             <Card style={styles.card}>
                 <Card.Title title="My Address" />
                 <Card.Content>
-                    <TextInput
-                        label="Country"
-                        value={country}
-                        onChangeText={setCountry}
-                        style={styles.input}
-                    />
+                    <Text style={styles.genderLabel}>Country</Text>
+                    <Picker
+                        selectedValue={country}
+                        onValueChange={(newValue, itemIndex) =>
+                            setCountry(newValue)
+                        }>
+                        {CountryList.map((list) => <Picker.Item label={list.name} key={list.code} value={list.name} />)}
+                    </Picker>
+
                     <TextInput
                         label="City"
                         value={city}
@@ -102,10 +133,10 @@ const Verification = () => {
                         value={postalCode}
                         onChangeText={setPostalCode}
                         style={styles.input}
+                        keyboardType='numeric'
                     />
                 </Card.Content>
             </Card>
-
 
             <Button mode="contained" style={styles.button} onPress={() => console.log('Form Submitted')}>
                 Submit
@@ -125,8 +156,8 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        padding: 16,
-
+        paddingHorizontal: 16,
+        paddingTop: 16,
     },
     card: {
         marginBottom: 16,
@@ -145,7 +176,7 @@ const styles = StyleSheet.create({
         marginBottom: 8,
     },
     button: {
-        marginVertical: 16,
+        marginBottom: 20,
     },
     uploadIdContainer: {
         // flexDirection: 'row',
